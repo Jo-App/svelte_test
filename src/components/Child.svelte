17f49569d525270c8d9c8ff4a64b3f2cd89c8aca
조성현 <josunghyun890@gmail.com>
2023-02-17 19:28:55 +0900

    미니 스타일링

--- src/components/Child.svelte
+++ src/components/Child.svelte
@@ -10,13 +10,31 @@
 
 </script>
 
-<main>
+<section>
   <h2>I'm Child !!!</h2>
-  <button on:click={childTalk}>Send to Parent</button>
-</main>
+  <button on:click={childTalk}>
+    <span class="material-symbols-outlined child-arrow">arrow_top_left</span>
+    Send to Parent
+  </button>
+</section>
 
 <style scoped>
+  section {
+    border: 1px solid green;
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 10px;
+  }
   h2 {
     color: green;
+  }
+  button {
+    width: 155px;
+  }
+  .child-arrow {
+    transform: rotate(90deg) scaleY(-1);
+    position: relative;
   }
 </style>
--- src/components/Parent.svelte
+++ src/components/Parent.svelte
@@ -14,14 +14,39 @@
   
 </script>
 
-<main>
+<section>
   <h2>I'm Parent!</h2>
-  <button on:click={callChild}>Send to Child</button>
+  <button on:click={callChild}>
+    Send to Child
+    <span class="material-symbols-outlined parent-arrow">arrow_top_left</span>
+</button>
   <Child bind:sayChild={sayChild}></Child>
-</main>
+</section>
 
 <style scoped>
+  section {
+    border: 1px solid red;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
   h2 {
     color: red;
+  }
+  button {
+    width: 155px;
+  }
+  .material-symbols-outlined {
+    font-variation-settings:
+    'FILL' 0,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 48,
+  }
+  .parent-arrow {
+    transform: rotate(90deg) scaleX(-1);
+    position: relative;
+    top: 10px;
   }
 </style>
